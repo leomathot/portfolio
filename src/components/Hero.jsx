6aceddefeb12d4b05fb5d6c5c69b8251ea378607
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-12-25 01:01:53 -0500

    Update content and style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <section className='hero'>
+      <div class="background"></div>
+      <div class="background-move"></div>
         <div>
             <h1><i class="fa-solid fa-chevron-right" /> <span>Leo</span>nardo Mathot</h1>
             <h2>Front End Devoloper</h2>
