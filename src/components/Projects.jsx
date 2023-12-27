dbdc9e8f17f87c926b313524c659b671a8edd5d0
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-12-27 15:54:55 -0500

    Update content and style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
-import RouterNavBar from './RouterNavBar'
-import ProjWeb from './ProjWeb'
-import ProjGames from './ProjGames'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Projects = () => {
   return (
-    <section className='max-width sect'>
-    <BrowserRouter>
+    <section id='projects' className='max-width sect'>
         <h2>Projects</h2>
-        <RouterNavBar />
-        <Routes>
-          <Route path='/' element={<ProjWeb/>}/>
-          <Route path='/2' element={<ProjGames/>}/>
-        </Routes>
-    </BrowserRouter>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident adipisci tempora numquam consectetur molestiae, ipsa iste velit unde a temporibus autem officia distinctio culpa accusamus assumenda quidem vitae sapiente suscipit?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident adipisci tempora numquam consectetur molestiae, ipsa iste velit unde a temporibus autem officia distinctio culpa accusamus assumenda quidem vitae sapiente suscipit?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident adipisci tempora numquam consectetur molestiae, ipsa iste velit unde a temporibus autem officia distinctio culpa accusamus assumenda quidem vitae sapiente suscipit?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident adipisci tempora numquam consectetur molestiae, ipsa iste velit unde a temporibus autem officia distinctio culpa accusamus assumenda quidem vitae sapiente suscipit?</p>
     </section>
   )
 }
