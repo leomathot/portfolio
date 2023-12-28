a6d14c129b12095659823872c4120668bb9d7f8b
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-12-28 01:17:41 -0500

    Update content and style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,23 @@ import React from 'react'
 const Hero = () => {
   return (
     <section id="about" className='hero min-h-[50vh] flex justify-center align-bottom items-center'>
-      <div class="bg-bottom"></div>
-      <div class="bg-move-1"></div>
-      <div class="bg-move-2"></div>
-      <div class="bg-top"></div>
-        <div>
-            <h1 className="text-2xl text-cyan-400 mb-2.5"><i class="fa-solid fa-chevron-right" /> <span>Leo</span>nardo Mathot</h1>
-            <h2>Front End Devoloper</h2>
-        </div>
+      <div className="bg-bottom"></div>
+      <div className="bg-move-1"></div>
+      <div className="bg-move-2"></div>
+      <div className="bg-top"></div>
+      <article>
+
+            <h1 className="text-2xl text-cyan-400 ml-[-28px] mb-2.5"><i class="fa-solid fa-chevron-right" /> <span>Leo</span>nardo Mathot</h1>
+
+            <h2 className='mb-4'>Front End Devoloper</h2>
+
+            <div className='text-cyan-400 flex gap-4'>
+              <i className="fa-brands fa-linkedin"></i>
+              <i className="fa-brands fa-github"></i>
+              <i className="fa-solid fa-envelope"></i>
+            </div>
+
+        </article>
     </section>
   )
 }
