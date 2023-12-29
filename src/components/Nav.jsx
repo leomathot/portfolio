61b75853fc038838da3398dc7dba3d6e5469c72c
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-12-29 03:26:33 -0500

    Update content and style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const liSty = 'opacity-70 hover:opacity-90 active:opacity-100'
+const liSty = 'opacity-70 hover:opacity-90 focus:opacity-100'
 
 const Nav = () => {
-  return (
-    <nav className='nav mb-10 bg-[var(--black)]'>
-        <ul className='flex justify-center gap-[2%]'>
-            <li className={liSty}>
-                <a className='p-2' href="#about">About</a>
-            </li>
-            <li className={liSty}>
-                <a className='p-2' href="#projects">Projects</a>
-            </li>
-            <li className={liSty}>
-                <a className='p-2' href="#contact">Contact</a>
-            </li>
-        </ul>
-    </nav>
-  )
+    const [sticky, setSticky] = useState('-2px')
+    let currPos = window.scrollY;
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY < currPos) {
+            // Scroll up -> show
+            setSticky('-2px');
+          } else {
+            // Scroll down -> hide
+            setSticky('-30px');
+          }
+          currPos = window.scrollY;
+        };
+        window.addEventListener('scroll', handleScroll);
+        // Cleanup function to remove the event listener
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, []);
+
+    return (
+        // <nav className={`nav mb-10 bg-[var(--black)] sticky top-[${sticky}] duration-500`}>
+
+        <nav 
+            className='nav mb-10 bg-[var(--black)]' 
+            style={{ position: 'sticky', top: sticky, transition: '0.4s' }}
+        >
+
+            <ul className='flex justify-center gap-[2%]'>
+                <li className={liSty}>
+                    <a className='p-2' href="#about">About</a>
+                </li>
+                <li className={liSty}>
+                    <a className='p-2' href="#projects">Projects</a>
+                </li>
+                <li className={liSty}>
+                    <a className='p-2' href="#contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Nav
