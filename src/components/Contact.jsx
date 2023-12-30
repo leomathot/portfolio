4b32d1e4a4316753ca060ef9a8f9b54a68dd1194
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2023-12-30 00:41:35 -0500

    Update content and style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,9 +6,10 @@ const Contact = () => {
         <h2 className='mb-10'>Contact</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt tempora aperiam, ab quae soluta corrupti placeat perspiciatis, quidem quas quibusdam minus voluptatem neque, doloribus nemo rem molestias quia. Earum, sapiente?</p>
 
-        <form action='https://formspree.io/f/mayzbkjj' method='POST'>
+        <form action='https://formspree.io/f/mayzbkjj' method='POST'
+          className='max-w-[500px] mx-auto mt-10'
+        >
 
-          <fieldset>
           <label htmlFor='name'>Name</label>
           <input id='name' name='name' type='text' required aria-required='true' />
 
@@ -18,8 +19,8 @@ const Contact = () => {
           <label htmlFor='message'>Message</label>
           <textarea id='message' name='message' required aria-required='true'/>
 
-          <input className='block bg-cyan-950 py-1 px-10' type='submit' value='Send' />
-          </fieldset>
+          <button className='bg-[var(--black)]' type='submit'>Send</button>
+          
         </form>
     </section>
   )
