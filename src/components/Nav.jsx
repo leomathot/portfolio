f084841c308213cf193ee826ace930bcc8363f1d
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2024-01-05 21:04:33 -0500

    Update style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,33 +2,51 @@ import React, { useState, useEffect } from 'react'
 
 const Nav = () => {
     const [stickyTop, setStickyTop] = useState('-2px')
+    const [relativeTop, setRelativeTop] = useState(0)
     let currPos = window.scrollY;
 
     useEffect(() => {
         const handleScroll = () => {
-          if (window.scrollY < currPos) {
-            // Scroll up -> show
-            setStickyTop('-2px')
-          } else {
-            // Scroll down -> hide
-            setStickyTop('-48px')
-          }
-          currPos = window.scrollY;
+            if (window.scrollY < currPos) {
+                // Scroll up -> show
+                setStickyTop('-2px')
+            } else {
+                // Scroll down -> hide
+                setStickyTop('-48px')
+            }
+            currPos = window.scrollY;
         };
         window.addEventListener('scroll', handleScroll)
         // Cleanup
         return () => window.removeEventListener('scroll', handleScroll)
-      }, []);
+    }, [])
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const navBar = document.querySelector('#nav')
+          const navBarTop = navBar.getBoundingClientRect().top
+      
+          if (navBarTop <= 30) {
+            setRelativeTop(0)
+          } else {
+            setRelativeTop(-20)
+          }
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, [])
 
     return (
-        <nav className='nav py-2 h-12 z-50 
+        <nav id='nav' className='nav py-2 h-12 z-50 
             bg-gradient-to-b 
             from-zinc-900 from-80%
             to-zinc-900/20
-            sticky duration-500' 
+            sticky duration-500'
             style={{ top: stickyTop }} /* doesn't work smoothly as a Tailwind class */
         >
-            <ul className='flex justify-center gap-[2%]'>
+            <ul className='flex justify-center gap-[2%] relative duration-500'
+                style={{ top: relativeTop }}
+            >
                 <li className='text-zinc-400 hover:text-zinc-50'>
                     <a className='p-2' href="#about">About</a>
                 </li>
