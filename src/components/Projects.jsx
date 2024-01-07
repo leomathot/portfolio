e4b1324ba0a4755b3d29003ec2143399cfd63e3e
Leo Mathot <113390921+leomathot@users.noreply.github.com>
2024-01-07 01:11:55 -0500

    Update content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,10 +11,13 @@ const Projects = () => {
       >
         <div className='max-width'>
           <h2 className='mb-10 text-lg font-[600] text-cyan-400'>Projects</h2>
-          <div className='py-1 mx-auto
-          grid  gap-10 w-[80%]
-          sm:grid-cols-2 sm:gap-x-6 sm:w-full
-          md:grid-cols-3'
+
+          {/* projects grid */}
+          <div className='
+            py-1 mx-auto
+            grid  gap-10 w-[80%]
+            sm:grid-cols-2 sm:gap-x-6 sm:w-full
+            md:grid-cols-3'
           >
             <Box title='Project 01' color='1' />
             <Box title='Project 02' color='2' />
@@ -23,6 +26,12 @@ const Projects = () => {
             <Box title='Project 05' color='2' />
             <Box title='Project 06' color='3' />
           </div>
+
+          {/* projects carousel */}
+          <div>
+
+          </div>
+
         </div>
       </section>
     </div>
